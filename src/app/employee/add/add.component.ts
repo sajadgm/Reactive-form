419d687ffede9ad7a4ebd187f9588666bc00bff0
Sajad Golshani <sajad.gm@gmail.com>
2021-12-28 12:09:04 +0330

    fix set date

--- src/app/employee/add/add.component.ts
+++ src/app/employee/add/add.component.ts
@@ -1,9 +1,7 @@
-import { EmployeeService } from './../../services/employee.service';
 import { IEmployee } from './../../interfaces/employee.interface';
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-add',
@@ -17,7 +15,6 @@ export class AddComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private dialog: MatDialogRef<AddComponent>,
-    private EmService: EmployeeService,
     @Inject(MAT_DIALOG_DATA) public data: IEmployee
   ) {}
 
